76049ac426ad7b3665f7fe301a4f247e608c1f76
Your Name <you@example.com>
2023-02-17 12:02:26 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (Success)
- */
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
-char ch;
+	char c = 'a';
 
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-return (0);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
